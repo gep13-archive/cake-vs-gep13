be057a0ebce751ed5ff30b22a51c7e06e712662d
Gary Ewan Park <gep13@gep13.co.uk>
2016-05-13 17:09:03 +0100

    Initial commit

---
+++ src/cake-vs/ContentType/CakeContentTypeDefinition.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Utilities;
+
+namespace cake_vs.ContentType
+{
+    public class CakeContentTypeDefinition
+    {
+        [Export(typeof(FileExtensionToContentTypeDefinition))]
+        [ContentType("CSharp")]
+        [FileExtension(".cake")]
+        public FileExtensionToContentTypeDefinition CakeFileExtension { get; set; }
+    }
+}
